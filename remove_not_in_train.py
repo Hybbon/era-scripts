20b6b9acfcff6994ae3b81d5592ccb5ec5b9f5a7
Samuel Oliveira <samuevan@gmail.com>
2016-11-28 10:03:03 -0200

    Modified remove_not_in_train in order to work with just train/test

--- remove_not_in_train.py
+++ remove_not_in_train.py
@@ -29,31 +29,47 @@ def ensure_same_users_items(train_data,val_data,test_data):
 
     ini = time.time()    
 
-    print("item train {0} items val {1}".format(len(train_data.item_id.unique()),len(val_data.item_id.unique())))
-    
     unique_items_train = set(train_data.item_id.unique())
-    
 
-    #Change the files in a way that the training file contains all possible items
-    unique_items = unique_items_train.union(val_data.item_id.unique())
-    left_sid = list(unique_items - unique_items_train)
+    if not val_data == None:
+        print("item train {0} items val {1}".format(len(train_data.item_id.unique()),len(val_data.item_id.unique())))
+                
+        #Change the files in a way that the training file contains all possible items
+        unique_items = unique_items_train.union(val_data.item_id.unique())
+        left_sid = list(unique_items - unique_items_train)
+        print(left_sid)
+        items_not_in_train = val_data['item_id'].isin(left_sid)        
+        train_data = train_data.append(val_data[items_not_in_train])
+        val_data = val_data[~items_not_in_train]
 
-    print(left_sid)
-
-    items_not_in_train = val_data['item_id'].isin(left_sid)
-
-    
-
-    train_data = train_data.append(val_data[items_not_in_train])
-    val_data = val_data[~items_not_in_train]
-
-
-    print("len items test : "+str(len(test_data.item_id.unique())))
-    test_data = test_data[test_data['item_id'].isin(unique_items)]
-    print("len items test : "+str(len(test_data.item_id.unique())))
+        print("len items test : "+str(len(test_data.item_id.unique())))
+        test_data = test_data[test_data['item_id'].isin(unique_items)]
+        print("len items test : "+str(len(test_data.item_id.unique())))
 
 
-    print("unique items time: "+ str(time.time()-ini))
+        print("unique items time: "+ str(time.time()-ini))
+
+
+
+    else:
+
+        unique_items = unique_items_train.union(test_data.item_id.unique())
+        left_sid = list(unique_items - unique_items_train)
+        print(left_sid)
+        items_not_in_train = test_data['item_id'].isin(left_sid)        
+        train_data = train_data.append(test_data[items_not_in_train])
+        print("len items test : "+str(len(test_data.item_id.unique())))
+        test_data = test_data[~items_not_in_train]
+        print("len items test : "+str(len(test_data.item_id.unique())))
+
+        '''print("len items test : "+str(len(test_data.item_id.unique())))
+        test_data = test_data[test_data['item_id'].isin(unique_items)]
+        print("len items test : "+str(len(test_data.item_id.unique())))
+
+
+        print("unique items time: "+ str(time.time()-ini))'''
+
+
 
     if len(left_sid) > 0:
         return train_data,val_data,test_data,True
@@ -71,18 +87,21 @@ if __name__ == "__main__":
     for p in range(1,6):
         part = 'u'+str(p)
         train = pd.read_csv(os.path.join(args.data,part+'.base'),sep="\t",names=header, dtype=types)
-        test = pd.read_csv(os.path.join(args.data,part+'.test'),sep="\t",names=header, dtype=types)
+        test = pd.read_csv(os.path.join(args.data,part+'.test'),sep="\t",names=header, dtype=types)        
         
-        validation = pd.read_csv(os.path.join(args.data,part+'.validation'),sep="\t",names=header, dtype=types)
-
+        if os.path.isfile(os.path.join(args.data,part+'.validation')):
+            validation = pd.read_csv(os.path.join(args.data,part+'.validation'),sep="\t",names=header, dtype=types)
+        else:
+            validation = None
+       
         train,validation,test,need_save =  ensure_same_users_items(train,validation,test)
 
 
         if need_save:
             print("Need to save the changes")
             train.to_csv(os.path.join(args.data,part+'.base'), header=False, index=False, sep="\t", columns=header)
-            validation.to_csv(os.path.join(args.data,part+'.validation'), header=False, index=False, sep="\t", columns=header)
             test.to_csv(os.path.join(args.data,part+'.test'), header=False, index=False, sep="\t", columns=header)    
-        
+            if not validation == None:
+                validation.to_csv(os.path.join(args.data,part+'.validation'), header=False, index=False, sep="\t", columns=header)
 
 
